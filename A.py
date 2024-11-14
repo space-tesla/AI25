c75ee7c1b000a9aec89f733fb7b86da8c71fa476
space-tesla <aspyrontech@gmail.com>
2024-11-14 23:49:50 +0530

    files added sucessfully !!!

--- A.py
+++ A.py
@@ -1,18 +1,13 @@
 #Q.1 Write a program to find and display perfect numbers from a list.
 
-#Program:
-
-# Function to check if a number is perfect
 def is_perfect(number):
     if number < 1:
         return False
     sum_of_divisors = sum(i for i in range(1, number) if number % i == 0)
     return sum_of_divisors == number
 
-# List of numbers
 numbers = [6, 28, 15, 23, 496, 12, 8128]
 
-# Find and display perfect numbers from the list
 perfect_numbers = [num for num in numbers if is_perfect(num)]
 print("Perfect numbers in the list:", perfect_numbers)
 
